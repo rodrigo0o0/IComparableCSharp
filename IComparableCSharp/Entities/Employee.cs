e86f9ac5902ddd70f8997fc343fa74e7a19db6c8
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-10 02:54:48 -0300

    Finish

---
+++ IComparableCSharp/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace IComparableCSharp.Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string employe)
+        {
+            string [] vect = employe.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1],CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + " R$ " + Salary.ToString("F2",CultureInfo.InvariantCulture) + " reais";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj != null)
+            {
+                Employee other = (Employee)obj;
+                return Salary.CompareTo(other.Salary);
+            }
+            else
+            {
+                throw new ArgumentException("Objeto nulo no CompareTo.");
+            }
+        }
+    }
+}
---
+++ IComparableCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using IComparableCSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IComparableCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string filePath = @"C:\temp\in.csv";
+            try
+            {
+                List<Employee> employees = new List<Employee>();
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        employees.Add(new Employee(sr.ReadLine()));                       
+                    }
+                }
+                employees.Sort();
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee);
+                }
+                Console.ReadKey();
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
